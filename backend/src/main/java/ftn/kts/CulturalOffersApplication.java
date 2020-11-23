344044f65285f41f11aa0986bc53cba79f459502
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2020-11-23 14:41:11 +0100

    #10 Initialize empty Spring Boot web project using start spring

---
+++ backend/src/main/java/ftn/kts/CulturalOffersApplication.java
@@ -0,0 +1,13 @@
+package ftn.kts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalOffersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ftn/kts/CulturalOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.kts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
