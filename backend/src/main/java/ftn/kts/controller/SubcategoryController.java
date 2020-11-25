ca5beae5cc89abcc7404508addab9a11aba51c79
stegnuti <stefanstegic@gmail.com>
2020-11-25 22:42:08 +0100

    #39 Implement repository, controller and service for Subcategory.

---
+++ backend/src/main/java/ftn/kts/controller/SubcategoryController.java
@@ -0,0 +1,10 @@
+package ftn.kts.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subcategories")
+public class SubcategoryController {
+
+}
