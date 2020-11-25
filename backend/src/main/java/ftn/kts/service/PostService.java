21667300510e6f35fb8f2b997f63af77e0361264
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-11-25 20:57:00 +0100

    #43 Add service, controller and repository for Post

---
+++ backend/src/main/java/ftn/kts/service/PostService.java
@@ -0,0 +1,16 @@
+package ftn.kts.service;
+
+import ftn.kts.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+}
