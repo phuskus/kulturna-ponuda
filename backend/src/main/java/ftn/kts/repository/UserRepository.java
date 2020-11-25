0bb53cb8253d0e4becdc2b5ba97b55503a9e5181
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-11-25 20:40:33 +0100

    #41 Add service, controller and repository for User

---
+++ backend/src/main/java/ftn/kts/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ftn.kts.repository;
+
+import ftn.kts.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
