4b4936e415b223c8f5a7186abe9a710858d71b93
Stefan <stefan.kandic@yahoo.com>
2020-12-02 20:41:24 +0100

    #52-T7 implement crud operations for Reviews

--- backend/src/main/java/ftn/kts/dto/ReviewDTO.java
+++ backend/src/main/java/ftn/kts/dto/ReviewDTO.java
@@ -9,7 +9,7 @@ public class ReviewDTO {
     private Long id;
 
     @NotBlank(message = "Rating is required!")
-    private String rating;
+    private Long rating;
 
     @NotBlank(message = "Content is required!")
     private String content;
@@ -17,11 +17,57 @@ public class ReviewDTO {
     private Long user;
     private Long culturalOffer;
 
+//    private Set<PicturesDTO> pictures;
+
 
     public ReviewDTO() {
-
     }
 
-    public ReviewDTO(Long id, String ratin, String conent, User user, CulturalOffer offer) {
+    public ReviewDTO(Long id, Long rating, String content, User user, CulturalOffer offer) {
+        this.id = id;
+        this.rating = rating;
+        this.content = content;
+        this.user = user.getId();
+        this.culturalOffer = offer.getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRating() {
+        return rating;
+    }
+
+    public void setRating(Long rating) {
+        this.rating = rating;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getUser() {
+        return user;
+    }
+
+    public void setUser(Long user) {
+        this.user = user;
+    }
+
+    public Long getCulturalOffer() {
+        return culturalOffer;
+    }
+
+    public void setCulturalOffer(Long culturalOffer) {
+        this.culturalOffer = culturalOffer;
     }
 }
