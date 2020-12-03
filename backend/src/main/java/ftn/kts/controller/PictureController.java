7d9d8fc076e5ad42a3e66f62646990307c8b27f0
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2020-12-03 01:57:33 +0100

    #51 Add POST-ing pictures

--- backend/src/main/java/ftn/kts/controller/PictureController.java
+++ backend/src/main/java/ftn/kts/controller/PictureController.java
@@ -1,10 +1,34 @@
 package ftn.kts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import ftn.kts.service.PictureService;
 
 @RestController
 @RequestMapping("/pictures")
 public class PictureController {
+	private PictureService service;
 
+	@Autowired
+	public PictureController(PictureService service) {
+		this.service = service;
+	}
+
+	@PostMapping
+	public ResponseEntity<Object> addPicture(@RequestParam(name = "file") MultipartFile file) {
+		try {
+			service.addPicture(file);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
--- backend/src/main/java/ftn/kts/service/PictureService.java
+++ backend/src/main/java/ftn/kts/service/PictureService.java
@@ -1,17 +1,44 @@
 package ftn.kts.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import ftn.kts.model.Picture;
 import ftn.kts.repository.PictureRepository;
 
 @Service
 public class PictureService {
-
+	private final String fileFolder = "\\pictures\\";
+	
     private PictureRepository pictureRepository;
 
     @Autowired
     public PictureService(PictureRepository pictureRepository) {
         this.pictureRepository = pictureRepository;
     }
+    
+    public void addPicture(MultipartFile file) throws IOException {
+		byte[] data = file.getBytes();
+		String fullPath = fileFolder + file.getOriginalFilename();
+		Path path = Paths.get(System.getProperty("user.dir") + fullPath);
+		Files.write(path,  data);
+		
+		Picture picture = new Picture(fullPath, file.getOriginalFilename());
+		pictureRepository.save(picture);
+    }
+
+	public Picture getOne(Long id) {
+		return pictureRepository.findById(id).get();
+	}
+
+	public List<Picture> getAll() {
+		return pictureRepository.findAll();
+	}
 }
