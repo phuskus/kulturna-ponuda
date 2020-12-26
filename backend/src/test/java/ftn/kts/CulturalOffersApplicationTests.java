a4f44ead36252cb28fe243635fc7135d848e13d2
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2020-12-26 14:03:00 +0100

    All tests passed

--- backend/src/test/java/ftn/kts/CulturalOffersApplicationTests.java
+++ backend/src/test/java/ftn/kts/CulturalOffersApplicationTests.java
@@ -1,32 +1,36 @@
 package ftn.kts;
 
-import ftn.kts.controller.*;
-import ftn.kts.service.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.TestPropertySource;
 
+import ftn.kts.controller.AdminControllerIntegrationTest;
+import ftn.kts.controller.CategoryControllerIntegrationTest;
 import ftn.kts.controller.CulturalOfferControllerIntegrationTest;
 import ftn.kts.controller.LoginControllerIntegrationTest;
 import ftn.kts.controller.PictureControllerIntegrationTest;
 import ftn.kts.controller.PostControllerIntegrationTest;
+import ftn.kts.controller.ReviewControllerIntegrationTest;
 import ftn.kts.repository.AdminRepositoryIntegrationTest;
 import ftn.kts.repository.AuthorityRepositoryIntegrationTest;
 import ftn.kts.repository.CategoryRepositoryIntegrationTest;
 import ftn.kts.repository.CulturalOfferRepositoryIntegrationTest;
 import ftn.kts.repository.ReviewRepositoryIntegrationTest;
 import ftn.kts.repository.SubcategoryRepositoryIntegrationTest;
+import ftn.kts.repository.SubscriptionRepositoryIntegrationTest;
 import ftn.kts.repository.UserRepositoryIntegrationTest;
 import ftn.kts.service.AdminServiceIntegrationTest;
 import ftn.kts.service.AuthorityServiceIntegrationTest;
 import ftn.kts.service.CategoryServiceIntegrationTest;
+import ftn.kts.service.CategoryServiceUnitTest;
 import ftn.kts.service.CulturalOfferServiceIntegrationTest;
 import ftn.kts.service.CulturalOfferServiceUnitTest;
 import ftn.kts.service.PictureServiceIntegrationTest;
 import ftn.kts.service.PostServiceIntegrationTest;
 import ftn.kts.service.ReviewServiceIntegrationTest;
 import ftn.kts.service.SubcategoryServiceIntegrationTest;
+import ftn.kts.service.SubscriptionServiceIntegrationTest;
 import ftn.kts.service.UserServiceIntegrationTest;
 
 @RunWith(Suite.class)
@@ -55,7 +59,9 @@ import ftn.kts.service.UserServiceIntegrationTest;
         SubcategoryRepositoryIntegrationTest.class,
         SubcategoryServiceIntegrationTest.class,
         PostServiceIntegrationTest.class,
-        PostControllerIntegrationTest.class
+        PostControllerIntegrationTest.class,
+        SubscriptionRepositoryIntegrationTest.class,
+        SubscriptionServiceIntegrationTest.class
 })
 @TestPropertySource("classpath:application.properties")
 class CulturalOffersApplicationTests {
