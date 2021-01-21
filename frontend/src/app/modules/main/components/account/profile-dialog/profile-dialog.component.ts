fdd02833b195e16db0832753484a24bb750c3508
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2021-01-21 17:47:12 +0100

    #137 User profile edit validation

--- frontend/src/app/modules/main/components/account/profile-dialog/profile-dialog.component.ts
+++ frontend/src/app/modules/main/components/account/profile-dialog/profile-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UserService } from 'src/app/services/user/user.service';
 import UpdateDialog from 'src/app/shared/dialog/UpdateDialog';
@@ -12,15 +13,27 @@ import { User } from 'src/app/shared/models/User';
 export class ProfileDialogComponent extends UpdateDialog<ProfileDialogComponent> {
   newObj: User;
 
+  profileForm: FormGroup;
+
   constructor(
+    private readonly fb: FormBuilder,
     public dialogRef: MatDialogRef<ProfileDialogComponent>,
     public service: UserService,
     @Inject(MAT_DIALOG_DATA) public data: User
   ) {
     super(dialogRef, service, data);
+    this.profileForm = this.fb.group({
+      username: [this.newObj.username],
+      name: [this.newObj.name, [Validators.required]],
+      surname: [this.newObj.surname, [Validators.required]]
+    })
    }
 
   ngOnInit(): void {
+  }
+
+  get f() {
+    return this.profileForm.controls;
   }
 
 }
