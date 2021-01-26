90686aeed32629b70b13a056e618fe004f2981af
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2021-01-26 11:20:03 +0100

    Login e2e tests; Admin button on main page

--- backend/src/main/java/ftn/kts/MockDataGenerator.java
+++ backend/src/main/java/ftn/kts/MockDataGenerator.java
@@ -249,6 +249,8 @@ public abstract class MockDataGenerator {
         UserService userService = applicationContext.getBean(UserService.class);
         ArrayList<UserDTO> userList = new ArrayList<>();
         UserDTO staticUser = new UserDTO("User", "Useric", "yahoo@yahoo.com", "12345");
+        UserDTO disabledUser = new UserDTO("User1", "Useric1", "yahoo1@yahoo.com", "12345");
+        userList.add(userService.create(disabledUser));
         userList.add(userService.createConfirmed(staticUser));
         for (int i = 0; i < REGISTERED_USER_COUNT; i++) {
             while (true) {
