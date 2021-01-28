b1a370f5e768a719b2a73ed9d5f2f66feb4e0581
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2021-01-28 17:50:33 +0100

    Edit profile slightly changed

--- frontend/src/app/modules/main/components/account/profile-dialog/profile-dialog.component.ts
+++ frontend/src/app/modules/main/components/account/profile-dialog/profile-dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import {
+  MessageService,
+  SnackbarColors,
+} from 'src/app/services/message/message.service';
 import { UserService } from 'src/app/services/user/user.service';
 import UpdateDialog from 'src/app/shared/dialog/UpdateDialog';
 import { User } from 'src/app/shared/models/User';
@@ -8,7 +13,7 @@ import { User } from 'src/app/shared/models/User';
 @Component({
   selector: 'app-profile-dialog',
   templateUrl: './profile-dialog.component.html',
-  styleUrls: ['./profile-dialog.component.scss']
+  styleUrls: ['./profile-dialog.component.scss'],
 })
 export class ProfileDialogComponent extends UpdateDialog<ProfileDialogComponent> {
   newObj: User;
@@ -18,21 +23,38 @@ export class ProfileDialogComponent extends UpdateDialog<ProfileDialogComponent>
     private readonly fb: FormBuilder,
     public dialogRef: MatDialogRef<ProfileDialogComponent>,
     public service: UserService,
+    public snack: MatSnackBar,
+    public messageService: MessageService,
     @Inject(MAT_DIALOG_DATA) public data: User
   ) {
     super(dialogRef, service, data);
     this.profileForm = this.fb.group({
       username: [this.newObj.username],
       name: [this.newObj.name, [Validators.required]],
-      surname: [this.newObj.surname, [Validators.required]]
-    })
-   }
-
-  ngOnInit(): void {
+      surname: [this.newObj.surname, [Validators.required]],
+    });
   }
+
+  ngOnInit(): void {}
 
   get f() {
     return this.profileForm.controls;
   }
 
+  onSubmit(): void {
+    if (this.profileForm.valid) {
+      this.newObj.name = this.profileForm.value['name'];
+      this.newObj.surname = this.profileForm.value['surname'];
+      this.service.update(this.newObj.id, this.newObj).subscribe(() => {
+        this.messageService.openSnackBar(
+          this.snack,
+          'Successfully updated profile!',
+          'End',
+          5000,
+          SnackbarColors.SUCCESS
+        );
+        this.dialogRef.close();
+      });
+    }
+  }
 }
